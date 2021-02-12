30a6adc8cf53ebd04e9f75db729678f1e777ccae
heyuanlong <1023417614@qq.com>
2021-02-12 23:21:34 +0800

    20

--- 20_decorator/decoratorx.go
+++ 20_decorator/decoratorx.go
@@ -3,13 +3,13 @@ package decorator
 type Component interface {
 	Calc() int
 }
-
+//---------------------------------------------
 type ConcreteComponent struct{}
 
 func (*ConcreteComponent) Calc() int {
 	return 0
 }
-
+//---------------------------------------------
 type MulDecorator struct {
 	Component
 	num int
@@ -25,19 +25,17 @@ func WarpMulDecorator(c Component, num int) Component {
 func (d *MulDecorator) Calc() int {
 	return d.Component.Calc() * d.num
 }
-
+//---------------------------------------------
 type AddDecorator struct {
 	Component
 	num int
 }
-
 func WarpAddDecorator(c Component, num int) Component {
 	return &AddDecorator{
 		Component: c,
 		num:       num,
 	}
 }
-
 func (d *AddDecorator) Calc() int {
 	return d.Component.Calc() + d.num
 }
